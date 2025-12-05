4c170e0a31f36725c4333338a3baa954bc61993d
Irina <trira2011@yandex.ru>
2025-12-05 07:35:38 +0300

    Create config.py

---
+++ TG01/TG02/config.py
@@ -0,0 +1,13 @@
+# Конфигурация Telegram-бота TG02
+
+# Токен Telegram-бота от BotFather
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+# API ключ от Google Translate (или используем библиотеку)
+# Для перевода используем google_translate_api или другую библиотеку
+
+# Директория для сохранения фото
+IMAGES_DIR = "img"
+
+# Директория для сохранения голосовых сообщений
+AUDIO_DIR = "audio"
